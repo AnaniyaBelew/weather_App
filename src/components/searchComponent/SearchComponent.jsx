1e73e6a505771b72f09b95e645f99d132a209c61
Ananiya <anewscho@gmail.com>
2023-10-06 15:16:24 +0300

    list countries

--- src/components/searchComponent/SearchComponent.jsx
+++ src/components/searchComponent/SearchComponent.jsx
@@ -1,5 +1,5 @@
 import '../searchComponent/SearchCompStyle.css'
-const Search=(countries)=>
+const Search=({countries,handelClick})=>
 {
     return(
         <div className='Search_container'>
@@ -7,19 +7,20 @@ const Search=(countries)=>
                 <input type='search' placeholder='Search for city'></input>
                 <button type='submit'>Search</button>
             </div>
-            <div>
-                <div>{countries}</div>
-                {/* {
-                    
-                    countries.map((country,idx)=>
-                    {
-                        return(
-                            <p key={idx}>
-                                country
-                            </p>
-                        )
-                    })
-                } */}
+            <div className='list_countries'>
+                {
+                    console.log(countries)
+                countries.countries.map((country,idx)=>
+                {
+                    return(
+                        <p key={idx}>
+                        <button  onClick={handelClick} value={country}>
+                            {country}
+                        </button>
+                        </p>
+                    )
+                })
+                } 
             </div>
         </div>
     )
